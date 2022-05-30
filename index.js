402b1df114ed7570dbe11155ab89150b8b11b1d1
Waleed Amjad <waleedamjad99@hotmail.com>
2022-05-30 21:23:57 +0500

    Apply Suggestions

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ const bookstore = new BookStore();
 const form = document.forms[0];
 const menuItems = document.querySelectorAll('.nav-link');
 
-function showSection() {
-  const show = this.getAttribute('data-section');
-  menuItems.forEach((menuItem) => { menuItem.classList.remove('active'); });
-  this.classList.add('active');
+menuItems.forEach((menuItem) => menuItem.addEventListener('click', () => {
+  const show = menuItem.getAttribute('data-section');
+  menuItems.forEach((menuItem) => { 
+    menuItem.classList.remove('active'); });
+    menuItem.classList.add('active');
   const toggleSections = document.querySelectorAll('.content-area');
   toggleSections.forEach((el) => {
     el.classList.add('d-none');
     document.getElementById(show).classList.remove('d-none');
   });
-}
-
-menuItems.forEach((menuItem) => menuItem.addEventListener('click', showSection));
+}));
 
 window.onload = () => { bookstore.displayBooks(); };
 
